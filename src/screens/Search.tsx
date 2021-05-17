8d10e222eb71a6fccd3867e64096b2324bfd47a0
Kent Claudio <rikysmile33@gmail.com>
2021-05-16 22:47:51 -0700

    Created Search screen

---
+++ src/screens/Search.tsx
@@ -0,0 +1,23 @@
+// third-party libraries
+import React from "react";
+
+// components
+
+// css
+import "../styles/Home.css";
+import "../styles/Universal.css";
+
+const Search = () => {
+  return (
+    <div className="searchContainer">
+      <div className="titleContainer verticalPadding">
+        <p>Search Page</p>
+      </div>
+      <div className="contentContainer verticalPadding">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Search;
