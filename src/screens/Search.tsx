0553137acb234359a95c74fc51d27cd8435fa5b3
Kent Claudio <rikysmile33@gmail.com>
2021-05-16 23:13:36 -0700

    Updated Search screen with SearchInput component

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 
 // components
+import SearchInput from "../components/SearchInput";
 
 // css
 import "../styles/Home.css";
@@ -14,7 +15,7 @@ const Search = () => {
         <p>Search Page</p>
       </div>
       <div className="contentContainer verticalPadding">
-        
+        <SearchInput />
       </div>
     </div>
   );
