407cac1b16a553af90d8c35ff6a85821a2d2378d
Kent Claudio <rikysmile33@gmail.com>
2021-05-16 20:04:50 -0700

    Updated NavBar component
    Replaced Link component with local NavLink component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,19 @@
 // third-party libraries
 import React from 'react'
 import { Link } from 'react-router-dom'
+import NavLink from './NavLink'
 
 // components
 
 
 // css
+import "../styles/NavBar.css"
 
 const NavBar = () => {
   return (
     <nav className="navBar">
-      <Link to="/">
-        Home
-      </Link>
-      <Link to="/about">
-        About
-      </Link>
+      <NavLink to="/" text="Home" />
+      <NavLink to="/about" text="About" />
     </nav>
   )
 }
