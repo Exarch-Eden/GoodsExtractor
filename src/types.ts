a1bf22c756f13f959f18b09295c8c020162da354
Kent Claudio <rikysmile33@gmail.com>
2021-06-02 11:21:40 -0700

    Updated types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,15 @@
 export interface Book {
-  id: string,
-  title?: string,
-  cover?: string,
-  artist?: string,
-  pages?: Pages,
+  id: string;
+  title?: string;
+  cover?: string;
+  artist?: string;
+  pages?: Pages;
+  thumbnails?: Pages;
 }
 
 export type Pages = string[];
+
+export interface SessionData {
+  id: string;
+  pages: Pages;
+}
