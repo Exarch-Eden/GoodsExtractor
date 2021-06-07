ba5605f24c3dd313b7b28f319b0927dc57a46440
Kent Claudio <rikysmile33@gmail.com>
2021-06-06 22:07:40 -0700

    Updated ImagePage for better performance
    Also added comments

--- src/screens/ImagePage.tsx
+++ src/screens/ImagePage.tsx
@@ -8,9 +8,10 @@ import PageNumbers from "../components/PageNumbers";
 
 // css
 import "../styles/Universal.css";
-import { SessionData } from "../types";
 
 // types
+import { Pages, SessionData } from "../types";
+
 type ImagePageRouteParams = {
   id: string;
   page: string;
@@ -25,10 +26,13 @@ const fetchImagePageData = async () => {
   console.log("fetchImagePageData()");
 };
 
-const dummyImageLink = "";
-
 const ImagePage = () => {
+  // holds the src value of the current image to display
   const [imageSrc, setImageSrc] = useState("");
+  // holds the src values of all the images belonging to the id-specified title
+  const [imageArray, setImageArray] = useState<Pages>([]);
+  // holds the maximum page number available for this specific title
+  // used mainly for pagination
   const [maxPageNumber, setMaxPageNumber] = useState(1);
 
   // for testing purposes
@@ -37,8 +41,12 @@ const ImagePage = () => {
 
   const { id, page }: ImagePageRouteParams = useParams();
 
+  // number parsed from stringified parameter value
   const pageNumber = parseInt(page);
 
+  // initial session data extraction triggered by change in id value
+  // id is representative of one title
+  // change in id (title) means change in entire images to display
   useEffect(() => {
     // attempt to get page url from session data
     const sessionDataStringified = window.sessionStorage.getItem("pages");
@@ -53,39 +61,46 @@ const ImagePage = () => {
         const sessionData: SessionData = JSON.parse(sessionDataStringified);
 
         // if the id from the session data matches that of the id passed as
-        // paramter from router,
-        // then extract the page url
+        // parameter from router, then extract the page url
         if (sessionData.id === id) {
           const index = pageNumber - 1;
           setImageSrc(sessionData.pages[index]);
           setMaxPageNumber(sessionData.pages.length);
-        }
+          setImageArray(sessionData.pages);
+        } // end if
       } catch (error) {
         console.log(error);
         console.log("Error attempting to extract data from session storage");
         console.log("Resorting to local server GET image request");
         fetchImagePageData();
-      }
+      } // end trycatch
     } else {
       console.log("failed to find session storage data");
       console.log("Resorting to local server GET image request");
 
       fetchImagePageData();
-    }
+    } // end ifelse
   }, [id]);
 
+  // updates image src whenever the page number is changed
   useEffect(() => {
-    // attempt to get page url from session data
-    const sessionDataStringified = window.sessionStorage.getItem("pages");
+    const index = pageNumber - 1;
+    setImageSrc(imageArray[index]);
 
-    if (sessionDataStringified) {
-      // session data string formatted to JSON
-      const sessionData: SessionData = JSON.parse(sessionDataStringified);
-      const index = pageNumber - 1;
-      setImageSrc(sessionData.pages[index]);
-    }
-  }, [pageNumber]);
+    // old code
+    // // attempt to get page url from session data
+    // const sessionDataStringified = window.sessionStorage.getItem("pages");
 
+    // if (sessionDataStringified) {
+    //   // session data string formatted to JSON
+    //   const sessionData: SessionData = JSON.parse(sessionDataStringified);
+    //   const index = pageNumber - 1;
+    //   setImageSrc(sessionData.pages[index]);
+    // }
+  }, [pageNumber, imageArray]);
+
+  // router path string lacking a second parameter
+  // used for pagination (component sets the second parameter value itself)
   const pathNoPageNumber = `/title/${id}/`;
 
   return (
